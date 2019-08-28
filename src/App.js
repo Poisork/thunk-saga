72f731304ffff2aee29777945dd26b62d910f348
oleg.kh <olegikhimich@gmail.com>
2019-08-28 16:24:02 +0300

    add redux dev tools

--- src/App.js
+++ src/App.js
@@ -12,11 +12,8 @@ function App(props) {
           : props.error
               ? <p>Error, try again</p>
               : <p><img alt='Dog if you fetch him' style={{width: '500px',height: '500px',objectFit: 'cover', borderRadius: '50%'}} src={props.url}/></p>}
-  </div>
+    </div>
   );
 }
 
-export default connect((state) => {
-  console.log(state);
-  return state;
-})(App);
+export default connect(store => store)(App);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,11 @@ import {createStore,applyMiddleware} from 'redux'
 import {reducer} from './reducer'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas/saga'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer,applyMiddleware(sagaMiddleware));
+const store = createStore(reducer,composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
 
